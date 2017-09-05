b082ad00df8fb621f33a9f99787b31e8465b6dcb
mihirthummar <mihirthummar@berkeley.edu>
2017-09-05 13:55:31 -0700

    Add files via upload

---
+++ reddit.py
@@ -0,0 +1,16 @@
+import praw
+reddit = praw.Reddit(client_id = 'Lq6zlWlS7RaVOQ', client_secret = 'yIZAzKj5JCEZCfhUQ-NTOirVVNY', username = 'mbkt8', password ='enter8898', user_agent = 'me')
+subreddit_news = reddit.subreddit('news')
+subreddit_nba = reddit.subreddit('nba')
+
+hot_news = subreddit_news.hot(limit = 20)
+hot_nba = subreddit_nba.hot(limit = 20)
+# print('NEWS:')
+# for submission in hot_news:
+# 	if not submission.stickied:
+# 		print(submission.title)
+print('NBA:')
+for submission in hot_nba:
+	if not submission.stickied:
+		print(submission.title)
+
